fa1e2aee5ff12ad97010bdba0f6f0265f5362629
knagamin <knagamin.hub@gmail.com>
2019-05-05 15:37:09 +0900

    implemented nilTracer

--- trace/tracer.go
+++ trace/tracer.go
@@ -13,11 +13,18 @@ type tracer struct {
 	out io.Writer
 }
 
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
 func (t *tracer) Trace(a ...interface{}) {
 	t.out.Write([]byte(fmt.Sprint(a...)))
 	t.out.Write([]byte("\n"))
 }
 
-func New(w io.Writer) Tracer {
-	return &tracer{out: w}
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+func Off() Tracer {
+	return &nilTracer{}
 }
