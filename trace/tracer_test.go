8a845fa4e04db551a7591b7662bbc946c24410ba
knagamin <knagamin.hub@gmail.com>
2019-05-05 12:37:08 +0900

    implement tracer struct and add its test code.

--- trace/tracer_test.go
+++ trace/tracer_test.go
@@ -1,9 +1,20 @@
 package trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
-	t.Error("まだテストを作成していません")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("return value from New() is nil")
+
+	} else {
+		tracer.Trace("Hello, trace package")
+		if buf.String() != "Hello, trace package\n" {
+			t.Errorf("'%s'という誤った文字が出力されました", buf.String())
+		}
+	}
 }
